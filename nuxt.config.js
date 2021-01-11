90a7e4ce70a8e3376c883a91c33c04f2a151fdf0
toshibowie86@gmail.com <toshibowie86@gmail.com>
2021-01-11 19:22:23 +0900

    Add fs module empty in nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,11 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    extend(config, ctx) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
   },
 
   // Currently there's an issue with static site generation and async functions
